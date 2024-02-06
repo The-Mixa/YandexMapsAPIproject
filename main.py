60260ed8893dce03c8b45bed7b7db5975870de8e
Slyvka_Nikita <slyvkanikita@ya.ru>
2024-02-06 18:47:59 +0500

    Добавлена функция поиска (ДЗ 2)

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from search_object import search_object
 from change_map_size import *
 import pygame
 import os
@@ -13,8 +14,33 @@ def show_map(params):
     pygame.display.set_caption('Большой Бублик')
     screen = pygame.display.set_mode((600, 450))
 
+    # размеры рамки ввода текста для поиска
+    input_bar_width = 240
+    input_bar_height = 23
+
+    input_text = ''
+    font_input = pygame.font.Font(None, 18)
+    font_input_text = font_input.render(input_text, True, 'black')
+
+    font_search = pygame.font.Font(None, 21)
+    font_search = font_search.render('Найти', True, 'white')
+
+    scale_font = pygame.font.Font(None, 20)
+    scale_text = scale_font.render(f'Масштаб: {scale}', True, 'black')
+
     while True:
         screen.blit(map_file, (0, 0))
+        screen.blit(scale_text, (1, 435))  # отрисовка масштаба
+
+        # отрисовка рамки поиска (с чёрной границей, размером в 1 пиксель)
+        pygame.draw.rect(screen, 'black', (9, 9, input_bar_width + 2, input_bar_height + 2))
+        pygame.draw.rect(screen, 'white', (10, 10, input_bar_width, 23))
+        screen.blit(font_input_text, (10, 15))
+
+        # отрисовка "кнопки поиска" (красный цвет) с текстом
+        pygame.draw.rect(screen, 'black', (15 + input_bar_width, 9, 50, input_bar_height + 2))
+        pygame.draw.rect(screen, 'red', (16 + input_bar_width, 10, 48, input_bar_height))
+        screen.blit(font_search, (15 + input_bar_width + 3, input_bar_height // 2 + 3))
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -22,13 +48,40 @@ def show_map(params):
                 pygame.quit()
                 exit(0)
 
-            # Обработка изменения масштабирования (PAGEUP - увеличение, PAGEDOWN - уменьшение):
+            # Обработка нажатия клавиатуры:
             elif event.type == pygame.KEYDOWN:
+
+                # Увеличение масштаба карты на 1:
                 if event.key == pygame.K_PAGEUP:
                     scale, params, map_file = change_map_size(scale, 1, params)
 
+                # Уменьшение масштаба карты на 1:
                 elif event.key == pygame.K_PAGEDOWN:
                     scale, params, map_file = change_map_size(scale, -1, params)
+
+                # Удаление 1 символа из строки поиска
+                elif event.key == pygame.K_BACKSPACE:
+                    input_text = input_text[:-1]
+
+                # Добавление 1 символа в строку поиска
+                else:
+                    input_text += event.unicode
+
+                # Постоянно обновляем строку поиска (отображаем только первые 28 символов запроса)
+                font_input_text = font_input.render(input_text[:28], True, 'black')
+                scale_text = scale_font.render(f'Масштаб: {scale}', True, 'black')
+
+            # Обработка нажатия ЛКМ (на клавишу поиска):
+            elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                x, y = pygame.mouse.get_pos()
+
+                # Выполняем поиск:
+                if x in range(15 + input_bar_width, 15 + input_bar_width + 50 + 1) and \
+                   y in range(9, 9 + input_bar_height + 2):
+                    try:
+                        params, map_file = search_object(input_text, params)
+                    except Exception as e:
+                        print('error', e)
 
             pygame.display.flip()
 
---
+++ search_object.py
@@ -0,0 +1,46 @@
+import requests
+from load_map import load_map
+
+
+def handler(address):
+    geocoder_api_server = "http://geocode-maps.yandex.ru/1.x/"
+
+    geocoder_params = {
+        "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
+        "geocode": address,
+        "format": "json"
+    }
+
+    response = requests.get(geocoder_api_server, params=geocoder_params)
+
+    # Преобразуем ответ в json-объект
+    json_response = response.json()
+    # Получаем первый топоним из ответа геокодера.
+    toponym = json_response["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]
+
+    # Координаты центра топонима:
+    toponym_coodrinates = toponym["Point"]["pos"]
+
+    # Долгота и широта:
+    toponym_longitude, toponym_lattitude = toponym_coodrinates.split(" ")
+
+    # Собираем параметры для запроса к StaticMapsAPI:
+    map_params = {
+        "ll": ",".join([toponym_longitude, toponym_lattitude]),
+        'z': 2,
+        "l": "map"
+    }
+
+    return map_params
+
+
+# Поиск объекта и его отображение
+def search_object(search, params):
+    _params = handler(search)
+    _params['z'] = params['z']
+
+    return _params, load_map(_params)
+
+
+
+
