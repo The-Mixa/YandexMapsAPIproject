8a2cd9590ccf4b0bc839e46c0b0e63a6b02e74c9
TOkotit <tokotit32@gmail.com>
2024-02-08 23:09:30 +0500

    11 чуть получше, но всё равно криво, но работает более менее

--- _11_find_adress.py
+++ _11_find_adress.py
@@ -1,13 +1,13 @@
 import requests
 def find_object(coords, window_coords, scale):
     map_coords = list(map(float, coords.split(',')))
-    click_coords = (round(scale[0] / 600  * window_coords[0], 6),  round(scale[1] / 450 * window_coords[1], 6))
-    crds_on_window = (str(float(map_coords[1] - scale[1] / 2 + click_coords[1])), str(map_coords[0] - scale[0] / 2 + click_coords[0]))
+    lat_point = map_coords[0] + (scale[0] / 600) * (window_coords[0] - 300) * 2
+    lon_point = map_coords[1] + (scale[1] / 450) * (225 - window_coords[1]) * 2
     search_api_server = "https://search-maps.yandex.ru/v1/"
     search_params = {
         "apikey": '182b9e48-9043-46cd-acc2-c50e9312a99d',
         "lang": "ru_RU",
-        "text": ','.join(crds_on_window),
+        "text": ','.join([str(lon_point), str(lat_point)]),
     }
     response = requests.get(search_api_server, params=search_params)
     json_response = response.json()
@@ -17,7 +17,6 @@ def find_object(coords, window_coords, scale):
     organization = json_response["features"][0]
     org_address = organization["properties"]["name"]
     org_cords = ','.join(map(str, organization['geometry']['coordinates']))
-
     return org_cords, org_address
 
 
